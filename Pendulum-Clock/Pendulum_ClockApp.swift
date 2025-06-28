49d523a3fab224ba8611bdf10bb30ddfa393ad30
Cong Le <longchike@gmail.com>
2025-06-27 23:35:51 -0700

    initial setup

---
+++ Pendulum-Clock/Pendulum_ClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pendulum_ClockApp.swift
+//  Pendulum-Clock
+//
+//  Created by Cong Le on 6/27/25.
+//
+
+import SwiftUI
+
+@main
+struct Pendulum_ClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
